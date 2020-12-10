1a5beb3b0ec5326e55a5d1b5a077b927f14ad379
N3smesharik <73171650+N3smesharik@users.noreply.github.com>
2020-12-10 18:12:42 +0700

    sdc

---
+++ Quick/Quick.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+#include <fstream>
+#include <ctime>
+#include <iomanip>
+#include <Windows.h>
+void swap(int* a, int* b)
+{
+    int f = *a;
+    *a = *b;
+    *b = f;
+}
+void quick(int* a, int n)
+{
+    int l = 0, r = n - 1, P = a[n / 2];
+    while (true)
+    {
+        while (a[l] < P || a[r] > P)
+        {
+            if (a[l] < P) l++;
+            if (a[r] > P) r--;
+        }
+        if (l <= r)
+        {
+            swap(&a[l], &a[r]);
+            l++;
+            r--;
+        }
+        if (l > r)
+            break;
+    }
+    if (r > 0)
+        quick(a, r + 1);
+    if (l < n)
+        quick(&a[l], n - l);
+}
+int main()
+{
+    std::ifstream file;
+    file.open("input.txt");
+    int n;
+    file >> n;
+    int* a = (int*)malloc(sizeof(int) * n);
+    for (int i = 0; i < n; i++)
+        file >> a[i];
+    int start = GetTickCount64();
+    quick(a, n);
+    start = GetTickCount64() - start;
+    std::ofstream file1;
+    file1.open("output.txt");
+    file1 << n << "\n";
+    for (int i = 0; i < n; i++)
+        file1 << a[i] << " ";
+    file1 << "\n" << start;
+    free(a);
+}
